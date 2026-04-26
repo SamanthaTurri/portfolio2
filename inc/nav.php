450505f9db0ccd9f7b8d54a069bc1148ccc511d1
Samantha Turri <94143532+SamanthaTurri@users.noreply.github.com>
2026-04-26 12:29:07 -0700

    Initial commit

---
+++ inc/nav.php
@@ -0,0 +1,99 @@
+
+<nav id="primary-nav" aria-label="Primary">
+  <section class="nav-section">
+    <div class="nav-row">
+      <h2>About</h2>
+      <ul class="card-nav">
+        <li class="card-nav-item">
+          <a class="card exp-card" href="/portfolio2/shopify.php" aria-label="Enterprise Experience: Shopify">
+            <div class="card-header">
+              <p class="org">Shopify</p>
+              <div class="pip" data-status="<?php echo getPipStatus('shopify.php') ?>" aria-hidden="true"></div>
+            </div>
+            <p class="card-title">Enterprise experience</p>
+            <div class="card-body">              
+              <div class="roles">
+                <p class="role">Developer</p>
+                <p class="role">Technical Analyst</p>
+                <p class="role">Escalated Support</p>
+              </div>
+            </div>
+          </a>
+        </li>
+
+        <li class="card-nav-item">
+          <a class="card exp-card" href="/portfolio2/goodsir.php" aria-label="Agency Experience: Goodsir">
+            <div class="card-header">
+              <p class="org">Goodsir</p>
+              <div class="pip" data-status="<?php echo getPipStatus('goodsir.php') ?>" aria-hidden="true"></div>
+            </div>
+            <p class="card-title">Agency experience</p>
+            <div class="card-body">
+              <div class="roles">
+                <p class="role">Senior Web Producer</p>
+              </div>
+              <p class="desc">Research, strategy, planning, client relations, project management</p>
+            </div>
+          </a>
+        </li>
+
+        <li class="card-nav-item">
+          <a class="card exp-card" href="/portfolio2/bcit.php" aria-label="Education: BCIT Front-End Web Developer Program">
+            <div class="card-header">
+              <p class="org">BCIT</p>
+              <div class="pip" data-status="<?php echo getPipStatus('bcit.php') ?>" aria-hidden="true"></div>
+            </div>
+            <p class="card-title">Graduate</p>
+            <div class="card-body">
+              <div class="roles">
+                <p class="role">Front-End Web Developer</p>
+              </div>
+              <p class="desc">UX design, HTML, CSS, PHP, WordPress, WooCommerce, React</p>
+            </div>
+          </a>
+        </li>
+
+        <li class="card-nav-item">
+          <a class="card exp-card" href="/portfolio2/human.php" aria-label="About me as a human being">
+            <div class="card-header">
+              <p class="org">British Columbia</p>
+              <div class="pip" data-status="<?php echo getPipStatus('human.php') ?>" aria-hidden="true"></div>
+            </div>
+            <p class="card-title">Human being</p>
+            <div class="card-body">
+              <div class="roles">
+                <p class="role">Myself</p>
+              </div>
+              <p class="desc">Computers, headphones, stages, opinions</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="nav-section">
+    <div class="nav-row">
+      <h2>Work</h2>
+      <ul class="card-nav">
+        <li class="card-nav-item">
+          <a class="card work-card" href="/portfolio2/vancity.php" aria-label="E-Commerce event sales project">
+            <div class="card-header">
+              <p class="org">Vancity Food Tours</p>
+              <div class="pip" data-status="<?php echo getPipStatus('vancity.php') ?>" aria-hidden="true"></div>
+            </div>
+            <p class="card-title">E-Commerce event sales</p>
+            <div class="card-body">
+              <div class="collaborators">
+                <img class="collaborator" src="img/collaborators/samantha.png" alt="Samantha Turri">
+                <img class="collaborator" src="img/collaborators/emily.png" alt="Emily Huang">
+                <img class="collaborator" src="img/collaborators/michelle.png" alt="Michelle Nguyen">
+              </div>
+              <p class="desc">Planning, management, programming</p>
+          </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+</nav>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once 'inc/progress.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/comeau-reset.css">
+  <link rel="stylesheet" href="css/nav.css">
+  <link rel="stylesheet" href="css/page.css">
+  <script src="js/progress.js" defer></script>
+  <title>Samantha Turri | Web Strategist, Developer &amp; Leader</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body class="debug">
+  <pre class="session-debug"><?php print_r($_SESSION); ?></pre>
+  <div id="wrapper">
+  <?php require_once 'inc/nav.php'; ?>
+  </div>
+</html>
+
+
